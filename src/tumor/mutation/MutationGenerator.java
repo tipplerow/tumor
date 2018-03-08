7cd49d26ad470a778f0ce13a597f9874c28df4f1
Scott Shaffer <jsshaff@berkeley.edu>
2018-03-08 09:55:44 -0800

    Adding an empty mutation generator

--- src/tumor/mutation/MutationGenerator.java
+++ src/tumor/mutation/MutationGenerator.java
@@ -23,4 +23,23 @@ public interface MutationGenerator {
      * @return the rate at which mutations arrive. 
      */
     public abstract MutationRate getMutationRate();
+
+    /**
+     * A mutator that always returns an empty mutation list.
+     */
+    public static final MutationGenerator EMPTY = EmptyGenerator.INSTANCE;
+}
+
+final class EmptyGenerator implements MutationGenerator {
+    private EmptyGenerator() {}
+
+    static final MutationGenerator INSTANCE = new EmptyGenerator();
+
+    @Override public MutationList generate() {
+        return MutationList.EMPTY;
+    }
+
+    @Override public MutationRate getMutationRate() {
+        return MutationRate.ZERO;
+    }
 }
