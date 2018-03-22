206f7e8212d5ff76bbd10fd4d2696d3c80e80aeb
tipplerow <jsshaff@berkeley.edu>
2018-03-22 06:56:06 -0700

    Change to the appropriate working directory

--- test/perfect-cell-lattice/test-perfect-cell-lattice.sh
+++ test/perfect-cell-lattice/test-perfect-cell-lattice.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cd `dirname $0`
+
 ${JAM_HOME}/bin/jam-test.sh \
            "${TUMOR_HOME}/bin/perfect-cell-lattice.sh perfect-cell-lattice.prop" \
            gold \
--- test/perfect-cell-point/test-perfect-cell-point.sh
+++ test/perfect-cell-point/test-perfect-cell-point.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cd `dirname $0`
+
 ${JAM_HOME}/bin/jam-test.sh \
            "${TUMOR_HOME}/bin/perfect-cell-point.sh perfect-cell-point.prop" \
            gold \
