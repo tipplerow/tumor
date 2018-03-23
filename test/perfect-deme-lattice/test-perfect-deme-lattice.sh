c8af104c40ede1c72b37e904eef489844c63f5e6
tipplerow <jsshaff@berkeley.edu>
2018-03-23 14:32:58 -0700

    Test perfect demes on a lattice

---
+++ test/perfect-deme-lattice/test-perfect-deme-lattice.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd `dirname $0`
+
+${JAM_HOME}/bin/jam-test.sh \
+           "${TUMOR_HOME}/bin/perfect-deme-lattice.sh perfect-deme-lattice.prop" \
+           gold \
+           cell-count-stat.csv \
+           cell-count-traj.csv
