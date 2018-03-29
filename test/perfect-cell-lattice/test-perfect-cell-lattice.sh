060f1cca3e470c7030cc5b064476ec51b23c2727
tipplerow <jsshaff@berkeley.edu>
2018-03-29 14:10:28 -0700

    Add final coordinates to regression test

--- test/perfect-cell-lattice/test-perfect-cell-lattice.sh
+++ test/perfect-cell-lattice/test-perfect-cell-lattice.sh
@@ -6,4 +6,5 @@ ${JAM_HOME}/bin/jam-test.sh \
            "${TUMOR_HOME}/bin/perfect-cell-lattice.sh perfect-cell-lattice.prop" \
            gold \
            cell-count-stat.csv \
-           cell-count-traj.csv
+           cell-count-traj.csv \
+           final-comp-coord.csv
