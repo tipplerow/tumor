297adef5d4ff47b9869ce0bfff8e6a8d1b2c9f87
tipplerow <jsshaff@berkeley.edu>
2018-07-06 14:14:38 -0700

    Do not inherit from scalar mutation to save the storage of the zero scalar coefficient

--- src/tumor/mutation/NeutralMutation.java
+++ src/tumor/mutation/NeutralMutation.java
@@ -7,16 +7,20 @@ import tumor.growth.GrowthRate;
  * Represents mutations that leave the growth rate unchanged
  * (passenger mutations).
  */
-public final class NeutralMutation extends ScalarMutation {
+public final class NeutralMutation extends Mutation {
     /**
      * Creates a new neutral mutation.
      */
     public NeutralMutation() {
-        super(0.0);
+        super();
     }
 
     @Override public GrowthRate apply(GrowthRate rate) {
         return rate;
+    }
+
+    @Override public boolean isIndependent() {
+        return true;
     }
 
     @Override public boolean isNeutral() {
